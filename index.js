ab7e6c6643957f97dbdd49bdddcc9f44983fce01
Muhammad Zawawi <kembedt@gmail.com>
2022-03-05 23:19:25 +0700

    add backend for todoApp

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/v1.0/", router);
-
 // test if deploy success
 app.get("/", (req, res) => res.send("Endpoint Todo App"));
+
+app.use("/api/v1.0/", router);
 
 const port = process.env.PORT || 5000;
 
